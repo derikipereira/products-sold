90b7b332ce973504d1997f034adb4d0a67062427
Deriki Pereira <derikipereira@gmail.com>
2023-11-20 12:28:44 -0300

    feat: :heavy_plus_sign: add cors

--- api-products-solds/src/main.ts
+++ api-products-solds/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
